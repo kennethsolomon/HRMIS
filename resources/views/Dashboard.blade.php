748c11ce7be05d13e50043b5ec73c70735358527
Kenneth <contact@kennethsolomon.com>
2020-12-21 22:08:58 +0800

    include necessary js files used by the template

--- resources/views/Dashboard.blade.php
+++ resources/views/Dashboard.blade.php
@@ -397,14 +397,14 @@
 	<!-- Vue -->
     <script src="js/app.js"></script>
     <!-- Bootstrap core JavaScript-->
-    {{-- <script src="vendor/jquery/jquery.min.js"></script> --}}
-    {{-- <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script> --}}
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    {{-- <script src="vendor/jquery-easing/jquery.easing.min.js"></script> --}}
+    <script src="js/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    {{-- <script src="js/sb-admin-2.min.js"></script> --}}
+    <script src="js/sb-admin-2.min.js"></script>
 
     <!-- Page level plugins -->
     {{-- <script src="vendor/chart.js/Chart.min.js"></script> --}}
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .js('resources/js/template/bootstrap.bundle.min.js', 'public/js')
+    .js('resources/js/template/jquery.easing.min.js' , 'public/js')
+    .js('resources/js/template/jquery.min.js' , 'public/js')
+    .js('resources/js/template/sb-admin-2.min.js' , 'public/js');
